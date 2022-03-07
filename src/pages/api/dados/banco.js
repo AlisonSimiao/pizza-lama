9f7b8282930cba96dc9617cffc6b01e39618a194
ALISON <superalison>
2022-03-07 20:26:13 -0300

    adiciona mais produtos e imagens

--- src/pages/api/dados/banco.js
+++ src/pages/api/dados/banco.js
@@ -1,46 +1,52 @@
 const produtos = [
     {
       id: 1,
-      img:"./IMG/pizza.png",
-      nome: "Normal",
+      img:"./IMG/pizza-banana.png",
+      nome: "Banana",
       preco: 1790
     },
     {
       id: 2,
-      img:"./IMG/pizza.png",
-      nome: "grande",
-      preco: 1000
+      img:"./IMG/pizza-calabresa.png",
+      nome: "calabresa",
+      preco: 1500
     },
     {
       id: 3,
-      img:"./IMG/pizza.png",
-      nome: "big big",
-      preco: 1000
+      img:"./IMG/pizza-california.png",
+      nome: "california",
+      preco: 2000
     },
     {
       id: 4,
-      img:"./IMG/pizza.png",
-      nome: "fome",
-      preco: 1000
+      img:"./IMG/pizza-camarão.png",
+      nome: "camarão",
+      preco: 2000
     },
     {
       id: 5,
-      img:"./IMG/pizza.png",
-      nome: "perto",
-      preco: 1000
+      img:"./IMG/pizza-cogumelo.png",
+      nome: "cogumelo",
+      preco: 1350
     },
     {
       id: 6,
-      img:"./IMG/pizza.png",
-      nome: "longe",
-      preco: 1000
+      img:"./IMG/pizza-Grande.png",
+      nome: "Grande",
+      preco: 2500
     },
     {
       id: 7,
+      img:"./IMG/pizza-pequena.png",
+      nome: "pequena",
+      preco: 1400
+    },
+    {
+      id: 8,
       img:"./IMG/pizza.png",
-      nome: "metade",
-      preco: 1000
-    }
+      nome: "classica",
+      preco: 1899
+    },
   ]
 
   const carrinho =[
